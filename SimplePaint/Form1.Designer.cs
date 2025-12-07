8f3dc3e024afa659072ef54b6abd95330e90762c
Radium-mr226 <wryotu@gmail.com>
2025-12-07 21:18:10 +0300

    Добавьте файлы проекта.

---
+++ SimplePaint/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Windows.Forms; // ПРАВИЛЬНО
+
+namespace SimplePaint
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btnEllipse = new System.Windows.Forms.Button();
+            this.btnRect = new System.Windows.Forms.Button();
+            this.btnLine = new System.Windows.Forms.Button();
+            this.btnEraser = new System.Windows.Forms.Button();
+            this.btnPen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.trackBarSize = new System.Windows.Forms.TrackBar();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.Gainsboro;
+            this.panelMenu.Controls.Add(this.btnEllipse);
+            this.panelMenu.Controls.Add(this.btnRect);
+            this.panelMenu.Controls.Add(this.btnLine);
+            this.panelMenu.Controls.Add(this.btnEraser);
+            this.panelMenu.Controls.Add(this.btnPen);
+            this.panelMenu.Controls.Add(this.btnSave);
+            this.panelMenu.Controls.Add(this.btnClear);
+            this.panelMenu.Controls.Add(this.trackBarSize);
+            this.panelMenu.Controls.Add(this.btnColor);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(950, 80);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // btnEllipse
+            // 
+            this.btnEllipse.Location = new System.Drawing.Point(470, 12);
+            this.btnEllipse.Name = "btnEllipse";
+            this.btnEllipse.Size = new System.Drawing.Size(60, 50);
+            this.btnEllipse.TabIndex = 8;
+            this.btnEllipse.Text = "Круг";
+            this.btnEllipse.UseVisualStyleBackColor = true;
+            this.btnEllipse.Click += new System.EventHandler(this.btnTool_Click);
+            // 
+            // btnRect
+            // 
+            this.btnRect.Location = new System.Drawing.Point(379, 12);
+            this.btnRect.Name = "btnRect";
+            this.btnRect.Size = new System.Drawing.Size(90, 50);
+            this.btnRect.TabIndex = 7;
+            this.btnRect.Text = "Квадрат";
+            this.btnRect.UseVisualStyleBackColor = true;
+            this.btnRect.Click += new System.EventHandler(this.btnTool_Click);
+            // 
+            // btnLine
+            // 
+            this.btnLine.Location = new System.Drawing.Point(314, 12);
+            this.btnLine.Name = "btnLine";
+            this.btnLine.Size = new System.Drawing.Size(64, 50);
+            this.btnLine.TabIndex = 6;
+            this.btnLine.Text = "Линия";
+            this.btnLine.UseVisualStyleBackColor = true;
+            this.btnLine.Click += new System.EventHandler(this.btnTool_Click);
+            // 
+            // btnEraser
+            // 
+            this.btnEraser.Location = new System.Drawing.Point(227, 12);
+            this.btnEraser.Name = "btnEraser";
+            this.btnEraser.Size = new System.Drawing.Size(86, 50);
+            this.btnEraser.TabIndex = 5;
+            this.btnEraser.Text = "Ластик";
+            this.btnEraser.UseVisualStyleBackColor = true;
+            this.btnEraser.Click += new System.EventHandler(this.btnTool_Click);
+            // 
+            // btnPen
+            // 
+            this.btnPen.Location = new System.Drawing.Point(136, 12);
+            this.btnPen.Name = "btnPen";
+            this.btnPen.Size = new System.Drawing.Size(90, 50);
+            this.btnPen.TabIndex = 4;
+            this.btnPen.Text = "Карандаш";
+            this.btnPen.UseVisualStyleBackColor = true;
+            this.btnPen.Click += new System.EventHandler(this.btnTool_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(860, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(78, 50);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Сохр.";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.Location = new System.Drawing.Point(776, 12);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(78, 50);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // trackBarSize
+            // 
+            this.trackBarSize.Location = new System.Drawing.Point(538, 36);
+            this.trackBarSize.Maximum = 30;
+            this.trackBarSize.Minimum = 1;
+            this.trackBarSize.Name = "trackBarSize";
+            this.trackBarSize.Size = new System.Drawing.Size(150, 56);
+            this.trackBarSize.TabIndex = 1;
+            this.trackBarSize.Value = 5;
+            this.trackBarSize.Scroll += new System.EventHandler(this.trackBarSize_Scroll);
+            // 
+            // btnColor
+            // 
+            this.btnColor.BackColor = System.Drawing.Color.Black;
+            this.btnColor.Location = new System.Drawing.Point(12, 12);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(100, 50);
+            this.btnColor.TabIndex = 0;
+            this.btnColor.Text = "Цвет";
+            this.btnColor.UseVisualStyleBackColor = false;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(950, 520);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 600);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panelMenu);
+            this.Name = "Form1";
+            this.Text = "Super Paint C#";
+            this.panelMenu.ResumeLayout(false);
+            this.panelMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBarSize;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnEllipse;
+        private System.Windows.Forms.Button btnRect;
+        private System.Windows.Forms.Button btnLine;
+        private System.Windows.Forms.Button btnEraser;
+        private System.Windows.Forms.Button btnPen;
+    }
+}
---
+++ SimplePaint/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+
+namespace SimplePaint
+{
+    public partial class Form1 : Form
+    {
+        // --- ПЕРЕМЕННЫЕ ---
+        private Bitmap _bitmap;           // Основной слой (сохраненный рисунок)
+        private Graphics _graphics;       // Графика для Bitmap
+        private Point _startPoint;        // Точка нажатия
+        private Point _currentPoint;      // Текущая точка мыши
+        private bool _isDrawing = false;
+
+        private Tool _currentTool = Tool.Pen; // Текущий инструмент
+        private Color _currentColor = Color.Black;
+        private int _currentSize = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDrawingSurface();
+        }
+
+        private void InitializeDrawingSurface()
+        {
+            // Создаем холст
+            int width = pictureBox1.Width > 0 ? pictureBox1.Width : 800;
+            int height = pictureBox1.Height > 0 ? pictureBox1.Height : 600;
+
+            _bitmap = new Bitmap(width, height);
+            _graphics = Graphics.FromImage(_bitmap);
+            _graphics.Clear(Color.White);
+            _graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            pictureBox1.Image = _bitmap;
+
+            // Настройка дефолтных значений UI
+            btnColor.BackColor = _currentColor;
+        }
+
+        // --- СОБЫТИЯ МЫШИ ---
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _isDrawing = true;
+            _startPoint = e.Location;
+            _currentPoint = e.Location; // Инициализируем, чтобы не было скачка
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!_isDrawing) return;
+
+            _currentPoint = e.Location;
+
+            // Если это Карандаш или Ластик - рисуем СРАЗУ в Bitmap
+            if (_currentTool == Tool.Pen || _currentTool == Tool.Eraser)
+            {
+                Pen p = GetCurrentPen();
+                _graphics.DrawLine(p, _startPoint, _currentPoint);
+                _startPoint = _currentPoint; // Перемещаем начало для следующего сегмента
+                p.Dispose(); // Обязательно освобождаем ресурсы пера
+            }
+
+            // Для фигур мы просто вызываем перерисовку экрана (сработает событие Paint)
+            // Это создаст эффект "превью" без сохранения в Bitmap
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (_isDrawing)
+            {
+                // Если рисовали фигуру, то в момент отпускания мыши 
+                // фиксируем её окончательно в Bitmap
+                if (_currentTool == Tool.Line || _currentTool == Tool.Rectangle || _currentTool == Tool.Ellipse)
+                {
+                    DrawShape(_graphics); // Рисуем в память
+                }
+            }
+            _isDrawing = false;
+        }
+
+        // --- ОТРИСОВКА ПРЕВЬЮ (Визуализация в реальном времени) ---
+
+        // Это событие вызывается каждый раз при pictureBox1.Invalidate()
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            // Если мы сейчас тянем мышь и рисуем ФИГУРУ
+            if (_isDrawing && (_currentTool == Tool.Line || _currentTool == Tool.Rectangle || _currentTool == Tool.Ellipse))
+            {
+                // Рисуем на экране (e.Graphics), а не в Bitmap
+                e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                DrawShape(e.Graphics);
+            }
+        }
+
+        // --- ВСПОМОГАТЕЛЬНЫЕ МЕТОДЫ ---
+
+        private Pen GetCurrentPen()
+        {
+            // Если ластик, цвет белый, иначе текущий
+            Color c = (_currentTool == Tool.Eraser) ? Color.White : _currentColor;
+            Pen p = new Pen(c, _currentSize);
+            p.StartCap = LineCap.Round;
+            p.EndCap = LineCap.Round;
+            return p;
+        }
+
+        private void DrawShape(Graphics g)
+        {
+
+            Pen p = GetCurrentPen();
+
+            if (_currentTool == Tool.Line)
+            {
+                g.DrawLine(p, _startPoint, _currentPoint);
+            }
+            else if (_currentTool == Tool.Rectangle)
+            {
+                Rectangle r = GetRect(_startPoint, _currentPoint);
+                g.DrawRectangle(p, r);
+            }
+            else if (_currentTool == Tool.Ellipse)
+            {
+                Rectangle r = GetRect(_startPoint, _currentPoint);
+                g.DrawEllipse(p, r);
+            }
+            p.Dispose();
+        }
+
+        // Метод для создания правильного прямоугольника (работает во все стороны)
+        private Rectangle GetRect(Point p1, Point p2)
+        {
+            int x = Math.Min(p1.X, p2.X);
+            int y = Math.Min(p1.Y, p2.Y);
+            int width = Math.Abs(p1.X - p2.X);
+            int height = Math.Abs(p1.Y - p2.Y);
+            return new Rectangle(x, y, width, height);
+        }
+
+        // --- UI СОБЫТИЯ ---
+
+        private void btnTool_Click(object sender, EventArgs e)
+        {
+            // Общий обработчик для кнопок инструментов
+            Button btn = sender as Button;
+            if (btn != null)
+            {
+                // Используем Tag кнопки или её имя для определения инструмента
+                // Здесь для простоты проверим текст или имя
+                switch (btn.Text)
+                {
+                    case "Карандаш": _currentTool = Tool.Pen; break;
+                    case "Ластик": _currentTool = Tool.Eraser; break;
+                    case "Линия": _currentTool = Tool.Line; break;
+                    case "Квадрат": _currentTool = Tool.Rectangle; break;
+                    case "Круг": _currentTool = Tool.Ellipse; break;
+                }
+            }
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                _currentColor = cd.Color;
+                btnColor.BackColor = _currentColor;
+            }
+        }
+
+        private void trackBarSize_Scroll(object sender, EventArgs e)
+        {
+            _currentSize = trackBarSize.Value;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            _graphics.Clear(Color.White);
+            pictureBox1.Invalidate();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "PNG|*.png|JPEG|*.jpg";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                _bitmap.Save(sfd.FileName);
+            }
+        }
+    }
+}
